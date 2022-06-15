69eb5dec35959b5d3158bdc6b525a971ba3d7c0e
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-06-14 22:11:49 -0300

    subindo código fonte
    subindo código fonte

---
+++ Desenho/app/src/main/java/com/example/desenho/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.desenho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+   private FrameLayout main;
+   private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        // obter o id da view
+        main = (FrameLayout) findViewById(R.id.main_view);
+        // adicionando a view da bola no tamanho x = 100, y = 100 e r =40;
+        main.addView(new Bola(this, 100, 100, 40));
+        main.addView(new Quadrado(this, 50, 25, 10, 20));
+        main.addView(new Reta(this,5,5,6,10));
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = main.getChildCount(); i >= 0; i--) {
+                    View vi = main.getChildAt(i);
+                    View vf = main.getChildAt(i);
+                    View vr = main.getChildAt(i);
+                    if (vi instanceof Bola) {
+                        main.removeViewAt(i);
+                    }
+                    if (vf instanceof Quadrado){
+                        main.removeViewAt(i);
+                    }
+                    if (vr instanceof Reta){
+                        main.removeViewAt(i);
+                    }
+                }
+            }
+
+        });
+
+            }
+
+
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                main.addView(new Bola(this,event.getX(),event.getY(), 40));
+                /*main.addView(new Quadrado(this,
+                        event.getX(),100, event.getX(),
+                         100));*/
+                main.addView(new Quadrado(this,event.getX(),event.getX()+100 , event.getY(), event.getY()+100));
+
+                main.addView(new Reta(this, event.getX()+200, event.getY()+200,event.getX()+400, event.getY()+200 ));
+
+                return true;
+
+            case MotionEvent.ACTION_MOVE:
+                return true;
+        }
+
+        return true;
+    }
+}
